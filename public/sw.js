b32e44cffe9e933ba689587ad2191f28c1ee05ec
rk7852430-ctrl <rk7852430@gmail.com>
2026-06-02 17:15:56 +0530

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,3 @@
 self.addEventListener('install', (e) => {
   console.log('App Installed Successfully!');
 });
-
-self.addEventListener('fetch', (e) => {
-  // Yeh file abhi basic installation ke liye hai
-});
